10ee992b63f98617c7939211e33257b3e107a870
James Miller <jmill358@charlotte.edu>
2026-07-24 13:07:25 -0400

    Create LegoItem class and first inventory object

---
+++ lego_item.py
@@ -0,0 +1,9 @@
+class LegoItem:
+    def __init__(self, set_name, set_number, theme, condition, complete, purchase_price, estimated_value):
+        self.set_name = set_name
+        self.set_number = set_number
+        self.theme = theme
+        self.condition = condition
+        self.complete = complete
+        self.purchase_price = purchase_price
+        self.estimated_value = estimated_value
---
+++ main.py
@@ -0,0 +1,6 @@
+from lego_item import LegoItem
+craggers = LegoItem("Craggers Command ship", 70006, "Chima", "used", False, 80, 70) 
+print("Set Name: " + craggers.set_name)
+print("Theme: " + craggers.theme)
+print("Purchased Price: ", craggers.purchase_price)
+print("Estimated Value: ", craggers.estimated_value)
